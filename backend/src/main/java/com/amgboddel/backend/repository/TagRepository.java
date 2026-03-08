b432b1f77b23d087679fb218f0fe8c8048419808
Toniomp <delahayetonio77@gmail.com>
2026-03-08 16:54:39 +0100

    ajout import

--- backend/src/main/java/com/amgboddel/backend/repository/TagRepository.java
+++ backend/src/main/java/com/amgboddel/backend/repository/TagRepository.java
@@ -3,7 +3,7 @@ package com.amgboddel.backend.repository;
 import com.amgboddel.backend.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 @Repository
